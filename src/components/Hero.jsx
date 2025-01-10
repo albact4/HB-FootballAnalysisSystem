6cff01afd3fcd76426a2588f568976901371fbb2
albact4 <albaclosat@gmail.com>
2025-01-10 14:37:07 +0100

    Add URL place holder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-            <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+            <img src={logo} alt='honeyball_logo' className='w-28 object-contain' />
             <button 
             type="button"
             onClick={() => window.open('https://github.com/albact4/AI-Scouting-Report')}
@@ -15,8 +15,8 @@ const Hero = () => {
 
         </nav>
         <h1 className='head_text'>
-            Get a Player Scouting Report with <br className='max-md:hidden' />
-            <span className='orange_gradient'> OpenAI GPT-4</span>
+            Get any Player Scouting Report with <br className='max-md:hidden' />
+            <span className='orange_gradient'> Honeyball</span>
         </h1>
         <h2 className='desc'>
             Get instant insights and recommendations of a player and see if it can fit your team with our AI-powered scouting report.
